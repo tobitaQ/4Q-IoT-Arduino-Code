0728c2dceda9b44bdc63b398591bc584e35e7c79
tobitaq <tobby@aiit.ac.jp>
2025-11-18 17:28:28 +0900

    LED test

---
+++ LED_test_copy_20251118172702/LED_test_copy_20251118172702.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);
+  Serial.println("HIGH");
+  delay(2000);
+  digitalWrite(13, LOW);
+  Serial.println("LOW");
+  delay(2000);
+}
