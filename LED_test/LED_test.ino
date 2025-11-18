85c96ca886de06db0c3750e9a613445b908b164f
tobitaq <tobby@aiit.ac.jp>
2025-11-18 13:00:02 +0900

    LED test

---
+++ LED_test/LED_test.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  Serial.begin(115200);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);
+  Serial.println("Hello World!!");
+  delay(2000);
+  digitalWrite(13, LOW);
+  delay(2000);
+}
